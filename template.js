1f717ccb5cbdd301a5fb24af9656d6e418307139
David Farago <david.farago@emarsys.com>
2023-12-01 10:45:11 +0100

    day 1

---
+++ template.js
@@ -0,0 +1,18 @@
+const input = ``
+
+const example = ``
+
+function part1(input) {
+
+}
+
+
+console.log("Part 1 expected:", , "actual: ", part1(example));
+console.log("Part 1 result:", part1(input));
+
+// function part2(input) {
+//
+// }
+//
+// console.log("Part 2 expected:", , "actual: ", part2(example2));
+// console.log("Part 2 result:", part2(input));
